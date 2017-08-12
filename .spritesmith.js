d3b581302cbd68e045ce0cdef501aeda18da707f
k-miyoshi <miyoshiez22@gmail.com>
2017-08-13 02:23:19 +0900

    gulpを廃止してnpm scriptsに以降しました

---
+++ .spritesmith.js
@@ -0,0 +1,6 @@
+module.exports = {
+    src: './src/sprites/*.png',
+    destImage: './docs/images/sprite.png',
+    destCSS: './src/sass/parts/_sprite.scss',
+    imgPath: '../images/sprite.png'
+}
